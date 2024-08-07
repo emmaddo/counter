aa034fb4d4c926988c6679d8f47f8a181018e56e
emmaddo <ermsapp@gmail.com>
2024-08-07 06:58:30 +0100

    Implemented interval-based counter with start and stop functionality

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 
 function App(){
+let [count, setCount] = useState(0);
+const [isCounting, setIsCounting] = useState(false);
 
-  let [count, setCount] = useState(0);
-
-  function increaseCount(){
-    if(count < 10){
-      setCount(count + 1);
-    }
+useEffect(() => {
+  if(isCounting){
+  const intervalId = setInterval(()=>{
+    setCount(prevCount => prevCount + 1);
+  }, 1000);
+  return ()=> {
+    clearInterval(intervalId);
   }
+} }
   
-  function decreaseCount(){
-    if(count > 0){
-      setCount(count - 1);
-    }
-  }
- useEffect(()=>{
-  console.log("A click occured");
- });
-  return(
+  ,[isCounting]);
+
+function startCount(){
+  setIsCounting(true);
+}
+
+function stopCount(){
+  setIsCounting(false);
+  setCount(0);
+}
+  return (
     <div>
-      <center><h2>Hello React!</h2>
-      <h3>{count}</h3>
-      <button onClick={increaseCount}>Increase Count</button >&nbsp;&nbsp;<button onClick={decreaseCount}>Decrease Count</button>
+      <center><h2>{count}</h2>
+      <button onClick={startCount}>Start Count</button>&nbsp;&nbsp;<button onClick={stopCount}>Stop Count</button>
       </center>
     </div>
-  ); 
+    
+  );
 }
 
 export default App;
