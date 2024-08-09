02a772d7556514356bb7ed872966af596eb5e19e
emmaddo <ermsapp@gmail.com>
2024-08-09 04:39:12 +0100

    Stage 2: Added Stop Button and Instruction, Questions Components

---
+++ src/instruction.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Instruction(){
+    return(
+        <div>
+            <center>
+                <h2>Read Carefully</h2>
+                <p>Attempt this task by Carefully studying and understanding each Question. </p>
+            </center>
+        </div>
+    );
+}
+
+export default Instruction;
---
+++ src/questions.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Questions(){
+    return(
+        <div>
+            <center>
+                    <p>
+                      <b>  Q1. List 10 States in the United States of America </b>
+                    </p>
+                    <p>
+                       <b> Answer:</b><br />
+                        <textarea placeholder="Enter Your Answer Here..." rows="4" cols="50">
+                            
+                        </textarea>
+                    </p>
+            </center>
+        </div>
+    );
+}
+
+export default Questions;
