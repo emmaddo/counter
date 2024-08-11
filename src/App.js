a5056a5eacb436c77341969409544630a7f0a0ca
emmaddo <ermsapp@gmail.com>
2024-08-11 06:55:45 +0100

    Add routing and navigation links to Instruction and Questions components

--- src/App.js
+++ src/App.js
@@ -1,36 +1,68 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
+import { Route, Routes, useNavigate, Link } from "react-router-dom"; 
+import Instruction from "./MyComponents/instruction"; 
+import Questions from "./MyComponents/questions"; 
 
-function App(){
-let [count, setCount] = useState(0);
-const [isCounting, setIsCounting] = useState(false);
+function App() {
+  const [count, setCount] = useState(10);
+  const [isCounting, setIsCounting] = useState(false);
+  const navigate = useNavigate(); 
 
-useEffect(() => {
-  if(isCounting){
-  const intervalId = setInterval(()=>{
-    setCount(prevCount => prevCount + 1);
-  }, 1000);
-  return ()=> {
-    clearInterval(intervalId);
+  useEffect(() => {
+    if (isCounting && count > 0) {
+      const intervalId = setInterval(() => {
+        setCount((prevCount) => prevCount - 1);
+      }, 1000);
+      return () => {
+        clearInterval(intervalId);
+      };
+    }
+  }, [isCounting, count]);
+
+  function startCount() {
+    setIsCounting(true);
+    navigate('/questions'); 
   }
-} }
-  
-  ,[isCounting]);
 
-function startCount(){
-  setIsCounting(true);
-}
+  function stopCount() {
+    setIsCounting(false);
+    setCount(0);
+  }
 
-function stopCount(){
-  setIsCounting(false);
-  setCount(0);
-}
   return (
     <div>
-      <center><h2>{count}</h2>
-      <button onClick={startCount}>Start Count</button>&nbsp;&nbsp;<button onClick={stopCount}>Stop Count</button>
+      <Routes>
+        <Route path="/" element={<InstructionPage />} />
+        <Route path="/questions" element={<QuestionsPage count={count} startCount={startCount} stopCount={stopCount} />} />
+      </Routes>
+    </div>
+  );
+}
+
+function InstructionPage() {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <Instruction />
+      <center>
+        <button onClick={() => navigate('/questions')}>Start Test</button>
       </center>
     </div>
-    
+  );
+}
+
+function QuestionsPage({ count, startCount, stopCount }) {
+  return (
+    <div>
+      <center>
+        <h2>{count}</h2>
+        <button onClick={startCount}>Start Test</button>&nbsp;&nbsp;
+        <button onClick={stopCount}>Stop Test</button>
+       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <Link to="/">Back Home</Link>
+      </center>
+      <Questions />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Instruction from './instruction';
-import Questions from './questions';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Instruction />
-    <App />
-    <Questions />
+    <BrowserRouter> {/* Wrap App with BrowserRouter */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
